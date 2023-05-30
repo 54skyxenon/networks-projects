595cb5f4b32c0179fd6c1f4af235a5ba7f89c731
Brandon Liang <54skyxenon@gmail.com>
2023-05-30 00:55:42 -0700

    Add HTTP client and multithreaded support

---
+++ web-server/http-client.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+# http-client.py
+
+from socket import *
+import sys
+
+def http_get(server_host, server_port, filename):
+    # Create a socket and connect to the server
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_host, server_port))
+
+    # Send the HTTP GET request
+    request = f'GET {filename} HTTP/1.1\r\nHost: {server_host}\r\n\r\n'
+    client_socket.sendall(request.encode())
+
+    # Receive and display the server's response
+    response = b''
+    while data := client_socket.recv(1024):
+        response += data
+
+    print(response.decode())
+
+    # Close the socket
+    client_socket.close()
+
+if __name__ == '__main__':
+    # Check if the command line arguments are provided
+    if len(sys.argv) != 4:
+        print('Usage: ./http-client.py server_host server_port filename')
+        exit(1)
+
+    # Parse the command line arguments
+    server_host = sys.argv[1]
+    server_port = int(sys.argv[2])
+    filename = sys.argv[3]
+
+    # Call the HTTP GET function
+    http_get(server_host, server_port, filename)    
---
+++ web-server/http-server.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python3
+# http-server.py
+
+from socket import *
+import threading
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 6789
+
+def recv_all(sock, buffer_size=1024):
+    '''Read until EOF in case buffer is too small.'''
+    data = b''
+
+    while part := sock.recv(buffer_size):
+        data += part
+        if len(part) < buffer_size:
+            break
+    
+    return data.decode()
+
+def handle_request(connection_socket):
+    try:
+        message = recv_all(connection_socket)
+        filename = message.split()[1]
+        print('Requested file:', filename)
+
+        with open(filename[1:], 'rb') as f:
+            # Send one HTTP header line into socket
+            connection_socket.sendall('HTTP/1.1 200 OK\r\n'.encode())
+            connection_socket.sendall('Content-Type: text/html\r\n'.encode())
+            connection_socket.sendall('\r\n'.encode())
+            # Send the content of the requested file to the client
+            connection_socket.sendall(f.read())
+    except IOError:
+        # Send response message for file not found
+        connection_socket.sendall('HTTP/1.1 404 Not Found\r\n'.encode())
+        connection_socket.sendall('Content-Type: text/plain\r\n'.encode())
+        connection_socket.sendall('\r\n'.encode())
+        # Send 404 and terminates program
+        connection_socket.sendall('404 Not Found'.encode())
+    
+    connection_socket.sendall('\r\n'.encode())
+    # Close client socket
+    connection_socket.close()
+
+if __name__ == '__main__':
+    # Prepare a server socket
+    with socket(AF_INET, SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server_socket.bind((SERVER_HOST, SERVER_PORT))
+        server_socket.listen()
+
+        while True:
+            print(f'Ready to serve with {threading.active_count()} threads...') 
+
+            # Establish the connection
+            connection_socket, addr = server_socket.accept()
+
+            # OPTIONAL EXERCISE 1: Service the client request in a separate thread
+            t = threading.Thread(target=handle_request, args=(connection_socket,))
+            t.start()
