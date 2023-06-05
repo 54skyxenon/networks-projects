075a73decda6fe11bb4b4e425cbec674f32bf5f4
Brandon Liang <54skyxenon@gmail.com>
2023-06-04 20:01:51 -0700

    Finish most of UDP Pinger

---
+++ udp-pinger/UDPPingerClient.py
@@ -0,0 +1,34 @@
+# UDPPingerClient.py
+
+from socket import *
+from datetime import datetime
+from statistics import mean
+
+NUM_REQUESTS = 10
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+rtt_data = []
+
+for sequence_number in range(1, NUM_REQUESTS + 1):
+    try:
+        request_time = datetime.now()
+        ping = f'Ping {sequence_number} {request_time.strftime("%H:%M:%S")}'
+
+        clientSocket.sendto(ping.encode(), ('127.0.0.1', 12000))
+        clientSocket.settimeout(1)
+        
+        message = clientSocket.recvfrom(1024)[0]
+        rtt = datetime.now() - request_time
+        rtt = round(rtt.total_seconds() * 1000, 3)
+        rtt_data.append(rtt)
+
+        print(f'{message.decode()}, RTT = {rtt} ms')
+    except timeout:
+        print('Request timed out')
+
+# Optional Exercise
+print('\n=== SUMMARY ===')
+print(f'Avg: {round(mean(rtt_data), 3)} ms, Min: {min(rtt_data)} ms, Max: {max(rtt_data)} ms')
+print(f'Packet loss: {100 - len(rtt_data) * 10}%')
---
+++ udp-pinger/UDPPingerServer.py
@@ -0,0 +1,29 @@
+# UDPPingerServer.py
+
+# We will need the following module to generate randomized lost packets
+from random import randint
+from socket import *
+
+# Create a UDP socket, using SOCK_DGRAM for UDP packets
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Assign IP address and port number to socket
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('127.0.0.1', 12000))
+
+while True:
+    # Generate random number in the range of 0 to 10
+    rand = randint(0, 10)
+    
+    # Receive the client packet along with the address it is coming from
+    message, address = serverSocket.recvfrom(1024)
+
+    # Capitalize the message from the client
+    message = message.upper()
+
+    # If rand is less is than 4, we consider the packet lost and do not respond
+    if rand < 4:
+        continue
+
+    # Otherwise, the server responds
+    serverSocket.sendto(message, address)
