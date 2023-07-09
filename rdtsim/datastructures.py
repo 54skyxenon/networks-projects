be44f6833859345933aaf3887add824e9ad5b84f
Brandon Liang <54skyxenon@gmail.com>
2023-07-08 20:07:32 -0700

    MVP for AB-protocol done

---
+++ rdtsim/datastructures.py
@@ -0,0 +1,35 @@
+###############################################################################
+
+## ************************* BASIC DATA STRUCTURES ****************************
+##
+## STUDENTS: Do not modify these definitions.
+##
+## ****************************************************************************
+
+# A Msg is the data unit passed from layer 5 (teacher's code) to layer 4
+# (student's code).  It contains the data (bytes) to be delivered to layer 5
+# via the student's transport-level protocol entities.
+#
+class Msg:
+    MSG_SIZE = 20
+
+    def __init__(self, data):
+        self.data = data                # type: bytes[MSG_SIZE]
+
+    def __str__(self):
+        return 'Msg(data=%s)' % (self.data)
+
+# A Pkt is the data unit passed from layer 4 (student's code) to layer 3
+# (teacher's code).  Note the pre-defined packet structure, which all students
+# must follow.
+#
+class Pkt:
+    def __init__(self, seqnum, acknum, checksum, payload):
+        self.seqnum = seqnum            # type: integer
+        self.acknum = acknum            # type: integer
+        self.checksum = checksum        # type: integer
+        self.payload = payload          # type: bytes[Msg.MSG_SIZE]
+
+    def __str__(self):
+        return ('Pkt(seqnum=%s, acknum=%s, checksum=%s, payload=%s)'
+                % (self.seqnum, self.acknum, self.checksum, self.payload))
